dcef39d3afd405410b88cc984b5fdba6e622f791
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-03-16 19:31:33 +0545

    admin login

--- register/login_process.php
+++ register/login_process.php
@@ -1,31 +1,34 @@
 <?php
-    session_start();
+session_start();
 
-    include '../database/db_connect.php'; 
-    
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-    
-        $sql = "SELECT type FROM login_credentials WHERE username = '$username' AND password = '$password'";
-        $result = $conn->query($sql);
-    
-        if ($result->num_rows > 0) {
-            $_SESSION['username'] = $username; 
-    
-            $row = $result->fetch_assoc();
-            $userType = $row['type'];
-    
-            if ($userType === 'student') {
-                header("Location: ../students/home.php?username=$username"); 
-                exit();
-            } elseif ($userType === 'expertise') {
-                header("Location: ../expertise/expertise_header.php?username=$username"); 
-                exit();
-            }
-            
-        } else {
-            echo "Invalid username or password";
+include '../database/db_connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT type FROM login_credentials WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['username'] = $username;
+
+        $row = $result->fetch_assoc();
+        $userType = $row['type'];
+
+        if ($userType === 'student') {
+            header("Location: ../students/home.php?username=$username");
+            exit();
+        } elseif ($userType === 'expertise') {
+            header("Location: ../expertise/expertise_header.php?username=$username");
+            exit();
+        } elseif ($userType === 'admin') {
+            header("Location: ../admin/admin_header.php?username=$username");
+            exit();
+
         }
+
+    } else {
+        echo "Invalid username or password";
     }
-    
+}
